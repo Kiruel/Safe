a33fed75ab8bfe999a83584ab8c32424c722e443
Safi NETTAH <snettah@e2r4p12.42.fr>
2016-02-27 17:07:18 +0100

    Ajout

--- SGApp/www/js/controllers.js
+++ SGApp/www/js/controllers.js
@@ -2,22 +2,8 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, Dservice) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
 
-  // Form data for the login modal
-  Dservice.getClientsTable(function(data) {
-    console.log(data);
-  });
-  $scope.data = {};
-  $scope.data.hygiene = {name: 'Hygiene', data: [45, 49, 60, 11, 66, 57, 49]};
-  $scope.data.sommeil = {name: 'Sommeil', data: [45, 12, 89, 81, 43, 54, 78]};
-  $scope.data.sante = {name: 'Santé', data: [43, 76, 78, 12, 34, 90, 43]};
-  $scope.data.alimentation = {name: 'Alimentation', data: [65, 59, 80, 81, 56, 55, 40]};
+
 
   $scope.loginData = {};
 
