86edee1d146943ff2dbca5957e6235528d293bad
Safi NETTAH <snettah@e2r4p12.42.fr>
2016-02-28 00:49:09 +0100

    Ajout

--- SGApp/www/js/ConsultingController.js
+++ SGApp/www/js/ConsultingController.js
@@ -1,11 +1,27 @@
 var consulting = angular.module('consulting.controllers', []);
 
-consulting.controller('consultingCtrl', function($scope, Dservice, SocketService) {
+consulting.controller('consultingCtrl', function($state, $scope, Dservice, SocketService) {
 
   console.log("Consulting controller");
-  SocketService.on('message', function(msg){
-    console.log(msg);
+  $scope.dataFromBanker = [];
+  var i = 0;
+  SocketService.on('push_from_banker', function(data){
+    if (data)
+    {
+      $state.go($state.current, {}, {reload: true});
+      $scope.dataFromBanker = [{i:data}].concat($scope.dataFromBanker);
+      i++;
+    }
   });
+
+//   Object {id: 0, type: "Test", duration: 2, projection: Object, analysis: "Test 22"…}
+// $$hashKey: "object:30"
+// analysis: "Test 22"
+// duration: 2
+// id: 0
+// projection: Object
+// type: "Test"
+// __proto__: Object
 
 
 });
--- SGApp/www/js/HomeController.js
+++ SGApp/www/js/HomeController.js
@@ -6,28 +6,40 @@ home.controller('homeCtrl', function($scope, Dservice) {
   $scope.data = [300, 500, 100];
   // Form data for the login modal
   $scope.client = {};
+  $scope.spinner = true;
 
   Dservice.getClientsTable(function(data) {
     $scope.client.prenom = data[36][2];
     $scope.client.clientId = data[36][0];
     $scope.client.risque = data[36][10];
+    $scope.client.comptes = [];
 
     console.log($scope.client);
     Dservice.getAccountsTable(function (data) {
-      console.log(data);
+      // console.log(data);
+      for(var i = 0; i < data.length; i++){
+        if (data[i][1] == $scope.client.clientId)
+        {
+          $scope.client.comptes.push(data[i][5]);
+          console.log($scope.client);
+        }
+      }
     })
   });
+
+
+setTimeout(function () {
 
   Highcharts.chart('container', {
          chart: {
              type: 'solidgauge',
-             marginTop: 50
+             marginTop: 70
          },
          title: {
-             text: 'Activity',
-             style: {
-                 fontSize: '24px'
-             }
+             text: '',
+            //  style: {
+            //      fontSize: '24px'
+            //  }
          },
          tooltip: {
              borderWidth: 0,
@@ -39,8 +51,8 @@ home.controller('homeCtrl', function($scope, Dservice) {
              pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}%</span>',
              positioner: function (labelWidth, labelHeight) {
                  return {
-                     x: 200 - labelWidth / 2,
-                     y: 180
+                     x: 50 + labelWidth / 2,
+                     y: 5
                  };
              }
          },
@@ -50,17 +62,17 @@ home.controller('homeCtrl', function($scope, Dservice) {
              background: [{ // Track for Move
                  outerRadius: '112%',
                  innerRadius: '88%',
-                //  backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0.3).get(),
+                 backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0.3).get(),
                  borderWidth: 0
              }, { // Track for Exercise
                  outerRadius: '87%',
                  innerRadius: '63%',
-                //  backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[1]).setOpacity(0.3).get(),
+                 backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[1]).setOpacity(0.3).get(),
                  borderWidth: 0
              }, { // Track for Stand
                  outerRadius: '62%',
                  innerRadius: '38%',
-                //  backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[2]).setOpacity(0.3).get(),
+                 backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[2]).setOpacity(0.3).get(),
                  borderWidth: 0
              }]
          },
@@ -82,31 +94,31 @@ home.controller('homeCtrl', function($scope, Dservice) {
          },
 
          series: [{
-             name: 'Move',
+             name: $scope.client.comptes[0],
              borderColor: Highcharts.getOptions().colors[0],
              data: [{
-                //  color: Highcharts.getOptions().colors[0],
+                 color: Highcharts.getOptions().colors[0],
                  radius: '100%',
                  innerRadius: '100%',
                  y: 80
              }]
          }, {
-             name: 'Exercise',
+             name: $scope.client.comptes[1],
              borderColor: Highcharts.getOptions().colors[1],
              data: [{
-                //  color: Highcharts.getOptions().colors[1],
+                 color: Highcharts.getOptions().colors[1],
                  radius: '75%',
                  innerRadius: '75%',
                  y: 65
              }]
          }, {
-             name: 'Stand',
+             name: $scope.client.comptes[2],
              borderColor: Highcharts.getOptions().colors[2],
              data: [{
-                //  color: Highcharts.getOptions().colors[2],
+                 color: Highcharts.getOptions().colors[2],
                  radius: '50%',
                  innerRadius: '50%',
-                 y: 50
+                 y: 90
              }]
          }]
      },
@@ -151,10 +163,7 @@ home.controller('homeCtrl', function($scope, Dservice) {
              })
             //  .translate(190, 96)
             //  .add(this.series[2].group);
-     });
-
-  // $scope.chartParams = {};
-
-  // $scope.chartParams.data = [300, 500, 100];
+     })
+   }, 3000);
 
 });
