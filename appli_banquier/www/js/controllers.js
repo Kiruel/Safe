1c0fad970419082c159e5ef5a862508fab24f1c5
z0mbie42 <b3ug.net@gmail.com>
2016-02-28 11:22:44 +0100

    sysy_oy

--- appli_banquier/www/js/controllers.js
+++ appli_banquier/www/js/controllers.js
@@ -7,7 +7,7 @@ angular.module('starter.controllers', [])
   setInterval(function() {
     $scope.notifications = Notifications.all();
   }, 200);
-  
+
   SocketService.on('accept_from_client', function(msg) {
     Notifications.append(msg);
   });
@@ -34,17 +34,75 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-    $scope.clients = Clients.all();
+  $scope.clients = Clients.all();
 })
 
 .controller('ProfileDetailCtrl', function($scope, $stateParams, Clients) {
   $scope.client = Clients.get($stateParams.profileId);
+  $(function () {
+    var seriesOptions = [],
+      seriesCounter = 0,
+        names = ['MSFT', 'AAPL', 'GOOG'];
+
+        function createChart() {
+          $('#container').highcharts('StockChart', {
+            rangeSelector: {
+              selected: 4
+            },
+
+            yAxis: {
+              labels: {
+                formatter: function () {
+                  return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                }
+              },
+              plotLines: [{
+                value: 0,
+                width: 2,
+                color: 'silver'
+              }]
+            },
+
+            plotOptions: {
+              series: {
+                compare: 'percent'
+              }
+            },
+
+            tooltip: {
+              pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+              valueDecimals: 2
+            },
+
+            series: seriesOptions
+          });
+        }
+
+        $.each(names, function (i, name) {
+
+          $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
+
+            seriesOptions[i] = {
+              name: name,
+              data: data
+            };
+
+            // As we're loading the data asynchronously, we don't know what order it will arrive. So
+            // we keep a counter and create the chart when all the data is loaded.
+            seriesCounter += 1;
+
+            if (seriesCounter === names.length) {
+              createChart();
+            }
+          });
+        });
+  });
 })
 
 .controller('OpportunitiesCtrl', function($scope, SocketService, Opportunities) {
 
   $scope.opportunities = Opportunities.all();
-   SocketService.on('message', function(msg){
+  SocketService.on('message', function(msg){
   });
 
   $scope.discardOpportunity = function(opportunity) {
