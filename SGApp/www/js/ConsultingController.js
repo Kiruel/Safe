06b7c0fd6c0609d49efe9e8d7107559bd53848c5
Safi NETTAH <snettah@e2r4p12.42.fr>
2016-02-28 14:01:23 +0100

    gogogogo

--- SGApp/www/js/ConsultingController.js
+++ SGApp/www/js/ConsultingController.js
@@ -18,34 +18,74 @@ consulting.controller('consultingCtrl', function($state, $scope, Dservice, Socke
   });
 
   $scope.acheter = function (id) {
-
     SocketService.emit('buy_product', {prod: id, action: true});
-    console.log(id);
-    console.log("Achter: "+id);
+    // console.log(id);
+    // console.log("Achter: "+id);
   }
 
   $scope.vendre = function (id) {
     SocketService.emit('buy_product', {prod: id, action: false});
-    
-    console.log("Vendre: "+id);
-    // $scope.dataFromBanker.remove(function(el) { return el.id === id; });
-
+    // console.log("Vendre: "+id);
   }
 
-  var removeCard = function(id) {
-
-  }
-
-
-
-//   Object {id: 0, type: "Test", duration: 2, projection: Object, analysis: "Test 22"…}
-// $$hashKey: "object:30"
-// analysis: "Test 22"
-// duration: 2
-// id: 0
-// projection: Object
-// type: "Test"
-// __proto__: Object
-
+  Highcharts.chart('containerChart', {
+        chart: {
+            type: 'area'
+        },
+        title: {
+            text: 'Historic and Estimated Worldwide Population Growth by Region'
+        },
+        subtitle: {
+            text: 'Source: Wikipedia.org'
+        },
+        xAxis: {
+            categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+            tickmarkPlacement: 'on',
+            title: {
+                enabled: false
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Billions'
+            },
+            labels: {
+                formatter: function () {
+                    return this.value / 1000;
+                }
+            }
+        },
+        tooltip: {
+            shared: true,
+            valueSuffix: ' millions'
+        },
+        plotOptions: {
+            area: {
+                stacking: 'normal',
+                lineColor: '#666666',
+                lineWidth: 1,
+                marker: {
+                    lineWidth: 1,
+                    lineColor: '#666666'
+                }
+            }
+        },
+        series: [{
+            name: 'Asia',
+            data: [502, 635, 809, 947, 1402, 3634, 5268]
+        }, {
+            name: 'Africa',
+            data: [106, 107, 111, 133, 221, 767, 1766]
+        }, {
+            name: 'Europe',
+            data: [163, 203, 276, 408, 547, 729, 628]
+        }, {
+            name: 'America',
+            data: [18, 31, 54, 156, 339, 818, 1201]
+        }, {
+            name: 'Oceania',
+            data: [2, 2, 2, 6, 13, 30, 46]
+        }]
+    });
 
 });
